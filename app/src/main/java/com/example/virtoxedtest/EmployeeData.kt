53f0669fd549e12ccfb42e7415812567267ea917
Atif-09 <ma2669637@gmail.com>
2022-12-24 17:22:34 +0500

    New

---
+++ app/src/main/java/com/example/virtoxedtest/EmployeeData.kt
@@ -0,0 +1,9 @@
+package com.example.virtoxedtest
+
+data class EmployeeData(
+    val id : Int,
+    val name : String,
+    val email : String,
+    val phone : String,
+    val salary : String,
+)
